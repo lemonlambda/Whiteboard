38a0afe7790249fde4f8b70361c3a10410af3804
LemonjamesD <lemon@lemonjamesd.com>
2023-09-12 23:38:26 -0400

    Implemented some of the stages and added commands struct

--- src/stages.c
+++ src/stages.c
@@ -1,13 +1,13 @@
 #include "stages.h"
+#include "string_replace.h"
 #include "vector.h"
 
 #include <string.h>
 #include <assert.h>
 
-void add_stage(stage_t *self, char *cmd) {
-    assert(cmd != NULL);
-
-    char *malloced = strdup(cmd);
+void add_stage(stage_t *self, command_t cmd) {
+    command_t *malloced = malloc(sizeof(command_t));
+    *malloced = cmd;
 
     vector_t *commands = &self->commands;
     commands->callbacks.push(commands, (void *)malloced);
@@ -27,20 +27,49 @@ void free_stage(stage_t self) {
 }
 
 // Default stages
-stage_t run_stage() {
-    stage_t stage = init_stage()
+stage_t build_stage(char *default) {
+    stage_t stage;
+    if (char == NULL) {
+        stage = init_stage("Build");
+    } else {
+        stage = init_stage(default);
+    }
 
     return stage;
 }
 
-stage_t build_stage() {
-    stage_t stage = run_stage()
+stage_t run_stage() {
+    stage_t stage = build_stage("Run");
+
+// Very fun and not annoying platform specific build instructions
+#ifdef __WIN32
+    stage.callbacks.add_stage(&stage, new_command("Make Dirs", "mkdir {srcdir} && mkdir {srcdir}\\{projectname} && mkdir {srcdir}\\{projectname}\\obj && mkdir {srcdir}\\{projectname}\\bin"));
+#else
+    stage.callbacks.add_stage(&stage, new_command("Make Dirs", "mkdir {srcdir} && mkdir {srcdir}/{projectname} && mkdir {srcdir}/{projectname}/obj && mkdir {srcdir}/{projectname}/bin"));
+#endif
 
     return stage;
 }
 
 stage_t clean_stage() {
-    stage_t stage = init_stage()
+    stage_t stage = init_stage("Clean");
     
     return stage;
+}
+
+
+command_t new_command(char *name, char *cmd) {
+    assert(name != NULL);
+    assert(cmd != NULL);
+    
+    command_t command;
+    command.name = strdup(name);
+    command.command = strdup(cmd);
+
+    return command;
+}
+
+void free_command(command_t cmd) {
+    free(cmd.name);
+    free(cmd.command);
 }
--- src/stages.h
+++ src/stages.h
@@ -2,21 +2,32 @@
 
 #include "vector.h"
 
+typedef struct command command_t;
+
 typedef struct stage {
     char *stage_name;
-    // Vector of char *
+    // Vector of command_t *
     vector_t commands;
 
     struct {
-        void (*add_stage)(struct stage *, char *);
+        void (*add_stage)(struct stage *, command_t );
     } callbacks;
 } stage_t;
 
+
 stage_t run_stage();
-stage_t build_stage();
+stage_t build_stage(char *default);
 stage_t clean_stage();
 
 stage_t init_stage(char *name);
-void add_stage(stage_t *self, char *cmd);
+void add_stage(stage_t *self, command_t cmd);
 
 void free_stage(stage_t self);
+
+typedef struct command {
+    char *name;
+    char *command;
+} command_t;
+
+command_t new_command(char *name, char *cmd);
+void free_command(command_t cmd);

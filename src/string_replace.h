5f1b85a278c3e0ab91efa58be59778bcd06641e3
LemonjamesD <lemon@lemonjamesd.com>
2023-09-12 22:31:43 -0400

    Added string replacing

---
+++ src/string_replace.h
@@ -0,0 +1,4 @@
+// The almighty stack-overflow gods blessed me with: https://stackoverflow.com/questions/779875/what-function-is-to-replace-a-substring-from-a-string-in-c
+#pragma once
+
+char *str_replace(char *orig, char *rep, char *with);

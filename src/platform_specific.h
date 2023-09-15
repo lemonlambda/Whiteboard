d96457b42709262702b4212cb88890569a7ef701
LemonjamesD <lemon@lemonjamesd.com>
2023-09-15 15:45:43 -0400

    Added dependant define to test windows without actually being on windows

---
+++ src/platform_specific.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// WARN: Keep this as uncommented for release builds
+// NOTE: You can comment this out if you need to test windows on linux for example
+#ifndef __WIN32
+    #define WIN32
+#endif

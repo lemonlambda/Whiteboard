6fdcddedd24a5b2d2ca08c5074e58d26e0445b08
Zephianah Serotonin <29110377+Zephia-Sero@users.noreply.github.com>
2023-09-20 17:15:32 -0500

    changed rust_types.h to use typedef, also included a helpful comment

--- src/rust_types.h
+++ src/rust_types.h
@@ -1,17 +1,20 @@
 #pragma once
 
 #include <stdlib.h>
+#include <stdint.h>
 
-#define f32 float
-#define f64 double
-#define f128 long double
-#define u8 uint8_t
-#define u16 uint16_t
-#define u32 uint32_t
-#define u64 uint64_t
-#define i8 int8_t
-#define i16 int16_t
-#define i32 int32_t
-#define i64 int64_t
-#define usize size_t
-#define isize ssize_t
+// NOTE: float, double, and long double are *NOT* guaranteed to be
+// 32, 64, and 128 bits respectively!
+typedef float f32;
+typedef double f64;
+typedef long double f128;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef size_t usize;
+typedef ssize_t isize;

ed3499449e8500a0443d0435e894a2ccbc4bd65a
Zephianah Serotonin <29110377+Zephia-Sero@users.noreply.github.com>
2023-09-20 20:06:04 -0500

    documented toml_format.h

--- src/toml_format.h
+++ src/toml_format.h
@@ -31,6 +31,19 @@ package_t init_package();
 // @arg(toml)	Whiteboard file toml
 void make_package(package_t *self, toml_table_t *toml);
 
+// @desc			Parsed format for entries in a whiteboard file
+// @field(default_bin)		Is this the default binary option?
+// @field(name)			Name of this configuration
+// @field(srcdir)		C source directory
+// @field(includedir)		C includes directory
+// @field(targetdir)		C target directory
+// @field(programincludedir)	Used for test bins to add include paths for their builds
+// @field(cc)			C Compiler to use
+// @field(ld)			Linker to use
+// @field(ccargs)		Args to use during compilation
+// @field(ldargs)		Args to use during linking
+// @field(callbacks)		Callback function container
+// @field(callbacks.print_bin)	DEBUG-only human-readable printer for `bin_t` objects
 typedef struct bin {
     bool default_bin;
     char *name;
@@ -50,11 +63,22 @@ typedef struct bin {
     } callbacks;
 } bin_t;
 
+// @desc	Create a new `bin_t`
+// @returns	A default `bin_t`
 bin_t init_bin();
 #ifdef DEBUG
+// @desc	Print a human-readable form of a `bin_t`
+// @arg(self)	Pointer to `bin_t` to print
 void print_bin(bin_t *self);
 #endif
 
+// @desc				Complete whiteboard configuration structure
+// @field(package)			`package_t` package information
+// @field(bin)				Vector of `bin_t` configurations for [[bin]] entries
+// @field(test)				Vector of `bin_t` configurations for [[test]] entries
+// @field(callbacks)			Callback function container
+// @field(callbacks.make_bin)		Function that creates a `bin_t` in `bin` or `test`, based on the name and whiteboard config
+// @field(callbacks.make_config)	Function that fills out `config_t` based on the whiteboard config
 typedef struct config {
     package_t package;
     // Vector of `bin_t`s
@@ -68,8 +92,21 @@ typedef struct config {
     } callbacks;
 } config_t;
 
+// @desc		Function that creates a `bin_t` in a `config_t`
+// @arg(self)		Config to apply change to
+// @arg(toml)		Whiteboard configuration table
+// @arg(bin_name)	Name of this `bin_t` configuration
 void make_bin(config_t *self, toml_table_t *toml, char *bin_name);
+// @desc	Function that fills out a `config_t`
+// @arg(self)	Config to apply change to
+// @arg(toml)	Whiteboard config table
 void make_config(config_t *self, toml_table_t *toml);
+// @desc	Create a new `config_t` object
+// @returns	A default `config_t`
 config_t init_config();
+// @desc	Free resources allocated by a `bin_t`
+// @arg(self)	`bin_t` to clean-up after
 void free_bin(bin_t const self);
+// @desc	Free resources allocated by a `config_t`
+// @arg(self)	`config_t` to clean-up after
 void free_config(config_t const self);

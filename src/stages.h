d9e381c4028c87d4affe4371bfb6a5cd180300d9
Zephianah Serotonin <29110377+Zephia-Sero@users.noreply.github.com>
2023-09-20 20:11:32 -0500

    Rearranged and documented command_t

--- src/stages.h
+++ src/stages.h
@@ -4,7 +4,23 @@
 #include "vector.h"
 #include "toml_format.h"
 
-typedef struct command command_t;
+// @desc		Structure holding information about a command
+// @field(name)		Human-friendly name of the command
+// @field(command)	Command to run, with replacement strings
+typedef struct command {
+    char *name;
+    char *command;
+} command_t;
+
+// @desc		Create a new command from `name` and `cmd`. Both will be run through `strdup`!
+// @arg(name)		Human-friendly name of the command
+// @arg(command)	Command to run, with replacement strings
+// @returns		New `command_t` with those components
+command_t new_command(char *name, char *cmd);
+// @desc	Frees up allocations caused by `strdup()` of `name` and `command`
+// @arg(cmd)	`command_t` to clean-up after
+void free_command(command_t cmd);
+
 
 typedef struct stage {
     char *stage_name;
@@ -29,13 +45,6 @@ void run_stages(stage_t *self, package_t *project, bin_t *bin);
 
 void free_stage(stage_t self);
 
-typedef struct command {
-    char *name;
-    char *command;
-} command_t;
-
-command_t new_command(char *name, char *cmd);
-void free_command(command_t cmd);
 
 char *get_source_files(bin_t* bin);
 char *replace_args(command_t *cmd, package_t *package, bin_t *bin);

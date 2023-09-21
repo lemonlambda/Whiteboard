ab9b301fa3ce1fdc4dd8f85251b72b2164a53be1
Zephianah Serotonin <29110377+Zephia-Sero@users.noreply.github.com>
2023-09-20 19:28:57 -0500

    documented args_parser.h functions

--- src/args_parser.h
+++ src/args_parser.h
@@ -29,12 +29,30 @@ typedef struct args {
     bool run_func;
 } args_t;
 
+// @desc	Provides a more useful string for printing, if the string is empty or NULL.
+// @arg(value)	Input string
+// @returns	If `value` is NULL, then "NULL".
+// @returns	If `strlen(value)` is 0, then "EMPTY_STRING".
+// @returns	Otherwise, `value`.
 const char *null_str(const char *value);
 
+// @desc	Creates a new `args_t`
+// @returns	Default `args_t`
 args_t init_args();
+// @desc	Cleans up memory allocated by use of `args_t`
 void free_args(args_t args);
+// @desc	Produces a human-readable form of `args_t`
+// @arg(args)	`args_t` object to convert
+// @returns	Stringified version of `args`
 char *to_string(args_t args);
 
+// @desc	Checks if `arg` doesn't match `"--"`. If so, duplicates `arg` to `args->build_name`.
+// @arg(arg)	Argument to check, should be `"--"` or a build name.
+// @arg(*args)	`arg_t` object to change, if `arg` isn't the default build.
+// @returns	True if default build, false otherwise.
 bool check_default_build(const char *arg, args_t *args);
+// @desc	Parses arguments into an `arg_t` format.
+// @arg(argc)	Length of argv array
+// @arg(argv)	Array of strings from command line arguments
+// @returns	An `args_t` object representing the command line arguments
 args_t parse_args(int argc, const char **argv);
-

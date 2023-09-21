dbf0e084fc79abb3d36cf87932b7a673dc987a7f
Zephianah Serotonin <29110377+Zephia-Sero@users.noreply.github.com>
2023-09-20 20:35:30 -0500

    documented run_bin.h

--- src/run_bin.h
+++ src/run_bin.h
@@ -3,5 +3,10 @@
 #include "args_parser.h"
 #include "toml_format.h"
 
+// @desc		Runs stage based on `args_t` provided
+// @arg(package)	Package config, passed to stage
+// @arg(bin)		Bin config, passed to stage
 void build_bin(package_t *package, bin_t *bin, args_t *args);
+// @desc		Runs whiteboard, parsing toml and such based on `args`
+// @arg(args)		`args_t` object to run on
 void run_whiteboard(args_t *args);
